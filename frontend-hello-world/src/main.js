7ad1da10b5c64ba7707914e9b92ca87563da4ede
Ruben Vanden Abeele <rubenvda@gmail.com>
2020-02-12 11:08:56 +0100

    Add demo graph

--- frontend-hello-world/src/main.js
+++ frontend-hello-world/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TempGraph from './TempGraph.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  render: h => h(TempGraph),
+}).$mount('#TempGraph')
