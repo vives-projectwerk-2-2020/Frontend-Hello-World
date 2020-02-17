2c0ca8cb98597d1ee4fc8bbca396cb3e771a911a
Sille Van Landschoot <info@sillevl.be>
2020-02-17 14:34:56 +0100

    Revert "refactor code"
    This reverts commit 46b71c25b8a9f02daa4e0601f0896e6bdeea30dd.

---
+++ src/TempChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="TempChart" >
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+  import LineChart from './Chart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Temperature',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    display: inline-block;
+    max-width: 500px;
+    margin:  150px auto;
+  }
+</style>
