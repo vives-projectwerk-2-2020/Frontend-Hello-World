5d68b658f07d842ca46e6cbc7dab37ca2d0473d2
Benjamin Dhondt <benjamin.dhondt@hotmail.com>
2020-02-21 16:06:35 +0100

    added

--- src/Administration.vue
+++ src/Administration.vue
@@ -1,18 +1,69 @@
 <template>
   <v-app id="inspire">
+      <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+         <v-list-item link>
+          <v-list-item-action>
+           <v-icon>mdi-log-in</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
     <v-content>
       <v-container
         class="fill-height"
         fluid
       >
         <v-row
-          align="left"
+          align="center"
+          justify="center"
+        >
+            <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
           justify="left"
         >
           <v-col
             cols="12"
             sm="8"
             md="4"
+            class=pr-5
           >
             <v-card class="elevation-12">
               <v-toolbar
@@ -51,10 +102,12 @@
               </v-card-actions>
             </v-card>
           </v-col>
+          
         <v-col
             cols="12"
             sm="8"
             md="4"
+            class=pl-5
           >
             <v-card class="elevation-12">
               <v-toolbar
@@ -101,6 +154,10 @@
         </v-row>
       </v-container>
     </v-content>
+        </v-row>
+      </v-container>
+    </v-content>
+
       <v-footer app>
       <span>&copy; 2019</span>
     </v-footer>
