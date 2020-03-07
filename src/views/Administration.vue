2858ab4a8d5eeb9880dc7d5c4ac3344bb4e7754c
Benjamin Dhondt <benjamin.dhondt@hotmail.com>
2020-03-07 12:36:04 +0100

    fixed clipping

--- src/views/Administration.vue
+++ src/views/Administration.vue
@@ -1,17 +1,15 @@
 <template>
-      <v-app id="inspire">
-        <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app temporary>
       <v-list dense>
-         <v-list-item link>
+        <v-list-item link>
           <v-list-item-action>
-           <v-icon>mdi-login</v-icon>
+            <v-icon>mdi-login</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title><router-link to="/login">Login/Registration</router-link></v-list-item-title>
+            <v-list-item-title>
+              <router-link to="/login">Login/Registration</router-link>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
@@ -33,151 +31,69 @@
       </v-list>
     </v-navigation-drawer>
     <v-container>
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-            <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="left"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-            class=pr-5
-          >
-          <v-container id="app">
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>User Login</v-toolbar-title>
-                <v-spacer />
-                <v-tooltip bottom>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <span>Codepen</span>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Username"
-                    name="login"
-                    type="text"
-                  />
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary">Login</v-btn>
-                <v-card-text>Forgot password?</v-card-text>
-              </v-card-actions>
-            </v-card>
-            </v-container>
-          </v-col>
-        <v-col
-            cols="12"
-            sm="8"
-            md="4"
-            class=pl-5
-          >
-          <v-container id="app">
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>User Registration</v-toolbar-title>
-                <v-spacer />
-                <v-tooltip bottom>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <span>Codepen</span>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Username"
-                    name="login"
-                    type="text"
-                  />
-
-                      <v-text-field
-                    label="Email"
-                    name="email"
-                    type="text"
-                  />
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>  
-                <v-btn color="primary">SIGN UP</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-container>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-        </v-row>
-      </v-container>
-    </v-content>
+      <v-content>
+        <v-content>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4" class="pr-5">
+              <div id="app">
+                <v-card class="elevation-12">
+                  <v-toolbar color="primary" dark flat>
+                    <v-toolbar-title>User Login</v-toolbar-title>
+                  </v-toolbar>
+                  <v-card-text>
+                    <v-text-field label="Username" name="login" type="text" />
+                    <v-text-field id="password" label="Password" name="password" type="password" />
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="primary">Login</v-btn>
+                    <v-btn text color="deep-blue accent-4">Forgot Password?</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="8" md="4" class="pl-5">
+              <v-container id="app">
+                <v-card class="elevation-12">
+                  <v-toolbar color="primary" dark flat>
+                    <v-toolbar-title>User Registration</v-toolbar-title>
+                  </v-toolbar>
+                  <v-card-text>
+                    <v-text-field label="Username" name="login" type="text" />
+                    <v-text-field label="Email" name="email" type="text" />
+                    <v-text-field id="password" label="Password" name="password" type="password" />
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="primary">SIGN UP</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-content>
+      </v-content>
     </v-container>
-       <v-app-bar
-      app
-      clipped-left
-    >
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Particula</v-toolbar-title>
     </v-app-bar>
-           <v-footer app>
+    <v-footer app>
       <span>&copy; Particula 2020</span>
     </v-footer>
-      </v-app>
+  </v-app>
 </template>
 
 <script>
 export default {
   props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-    created () {
-      this.$vuetify.theme.dark = false
-    },
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  }),
+  created() {
+    this.$vuetify.theme.dark = false;
   }
+};
 </script>
 
 <style>
