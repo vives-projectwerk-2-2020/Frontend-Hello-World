e2a813704ea6d5b78e71dc16b48884857757e2d3
Matthias <r758230@student.vives.com>
2020-03-11 11:42:43 +0100

    AboutPage layout done

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,7 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
-         <v-list-item link>
-          <v-list-item-action>
-           <v-icon>mdi-login</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title><router-link to="/login">Login/Registration</router-link></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
         <v-list-item link>
           <v-list-item-action>
             <v-icon>mdi-home</v-icon>
@@ -24,53 +12,144 @@
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-information-outline</v-icon>
+            <v-icon>mdi-contact-mail</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>About</v-list-item-title>
+            <v-list-item-title>Contact</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app clipped-left>
+    <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>About</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
+        <p class="display-2">About the Project</p>
+
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-img src="../img/Image_1.jpg" :aspect-ratio="16/9"></v-img>
+          </v-col>
+
+          <v-col cols="12" sm="6">
+            <v-img src="../img/Image_2.jpg" :aspect-ratio="16/9">
+              <div class="fill-height bottom-gradient"></div>
+            </v-img>
           </v-col>
         </v-row>
+
+        <p class="subtitle-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus similique odio incidunt asperiores? Porro dolorem incidunt culpa! Incidunt illo vel animi
+          laudantium numquam quae similique, maiores ea pariatur, repellendus corporis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eos dolorum quos
+          quas, officia, dignissimos deserunt perferendis laudantium ab nihil aliquam corrupti expedita quibusdam quia natus eveniet recusandae culpa voluptatem. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Hic, tenetur! Exercitationem, commodi neque! Laudantium enim eius quis ea? Quod totam magni tempore
+          consequuntur alias omnis earum quisquam sunt hic. Ducimus. orem ipsum dolor sit amet consectetur adipisicing elit. A illum labore quod voluptatem cumque
+          laudantium deleniti ea maiores quas. Debitis suscipit, quasi ipsa non molestias eveniet minima rem doloribus laborum. adipisicing elit. Amet eos dolorum quos
+          quas, officia, dignissimos deserunt perferendis laudantium ab nihil aliquam corrupti expedita quibusdam quia natus eveniet recusandae culpa voluptatem. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Hic, tenetur! Exercitationem, commodi neque! Laudantium enim eius quis ea? Quod totam magni tempore
+          consequuntur alias omnis earum quisquam sunt hic. Ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. A illum labore quod voluptatem cumque
+          laudantium deleniti ea maiores quas. Debitis suscipit, quasi ipsa non molestias eveniet minima rem doloribus laborum.
+        </p>
+
+        <p class="display-1">Used materials and sensors</p>
+
+        <v-row>
+          <v-col cols="6" sm="4">
+            <v-img src="../img/Image_3.jpg" :aspect-ratio="16/9"></v-img>
+
+            <p class="body-1">
+              orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita!
+            </p>
+          </v-col>
+
+          <v-col cols="6" sm="4">
+            <p class="body-1">
+              orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis quam
+              quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita!
+            </p>
+            <v-img src="../img/Image_4.jpg" :aspect-ratio="16/9">
+              <div class="fill-height bottom-gradient"></div>
+            </v-img>
+          </v-col>
+
+          <v-col cols="6" sm="4">
+            <v-img src="../img/Image_5.jpg" :aspect-ratio="16/9">
+              <div class="fill-height repeating-gradient"></div>
+            </v-img>
+            <p class="body-1">
+              orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis quam
+              quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita! Deserunt maiores commodi atque at itaque rerum, sequi fuga quos, cumque fugiat qui ab perferendis
+              quam quia reprehenderit animi blanditiis tenetur expedita!
+            </p>
+          </v-col>
+        </v-row>
+
+        <p class="display-1">Extra information</p>
+
+        <p class="body-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita ut ipsum saepe earum, enim rem adipisci quas neque reiciendis impedit, iure inventore,
+          totam quod. Molestiae consequuntur quibusdam eveniet ab reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, perferendis
+          unde. Aliquid ratione iure, ullam assumenda harum aliquam nihil sapiente sunt voluptatem dignissimos, veniam amet quae voluptatum, itaque fuga esse.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam quae laudantium earum natus. Quo totam deleniti hic quibusdam laboriosam, veritatis
+          debitis illum, sapiente consequatur esse, voluptate numquam! Vel, inventore. Vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse eveniet,
+          quibusdam voluptatibus commodi, blanditiis facere, qui debitis porro ipsam iste fuga mollitia dignissimos magnam assumenda harum ratione consequuntur
+          repudiandae sunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate modi quo corporis libero, aperiam expedita esse saepe? Nam
+          repudiandae deleniti impedit suscipit laboriosam possimus qui? Ipsa, provident? Corrupti, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Similique, accusantium delectus consequatur, maiores tenetur laboriosam quam libero maxime vitae ab facilis distinctio eos eum, obcaecati architecto
+          voluptatum rem in velit.
+        </p>
+
+        <p class="display-1">Vives</p>
+
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-img src="../img/Image_6.jpg" :aspect-ratio="16/9">
+              <div class="fill-height repeating-gradient"></div>
+            </v-img>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <p class="body-1">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita ut ipsum saepe earum, enim rem adipisci quas neque reiciendis impedit, iure inventore,
+              totam quod. Molestiae consequuntur quibusdam eveniet ab reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, perferendis
+              unde. Aliquid ratione iure, ullam assumenda harum aliquam nihil sapiente sunt voluptatem dignissimos, veniam amet quae voluptatum, itaque fuga esse.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam quae laudantium earum natus. Quo totam deleniti hic quibusdam laboriosam, veritatis
+              debitis illum, sapiente consequatur esse, voluptate numquam! Vel, inventore. Vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse eveniet,
+              quibusdam voluptatibus commodi, blanditiis facere, qui debitis porro ipsam iste fuga mollitia dignissimos magnam assumenda harum ratione consequuntur
+              repudiandae sunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate modi quo corporis libero, aperiam expedita esse saepe? Nam
+              repudiandae deleniti impedit suscipit laboriosam possimus qui? Ipsa, provident? Corrupti, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Similique, accusantium delectus consequatur, maiores tenetur laboriosam quam libero maxime vitae ab facilis distinctio eos eum, obcaecati architecto
+              voluptatum rem in velit.
+            </p>
+          </v-col>
+        </v-row>
+
+        <p class="display-1">Our experiences</p>
+        <p class="body-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita ut ipsum saepe earum, enim rem adipisci quas neque reiciendis impedit, iure inventore,
+          totam quod. Molestiae consequuntur quibusdam eveniet ab reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, perferendis
+          unde. Aliquid ratione iure, ullam assumenda harum aliquam nihil sapiente sunt voluptatem dignissimos, veniam amet quae voluptatum, itaque fuga esse.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam quae laudantium earum natus. Quo totam deleniti hic quibusdam laboriosam, veritatis
+          debitis illum, sapiente consequatur esse, voluptate numquam! Vel, inventore. Vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse eveniet,
+          quibusdam voluptatibus commodi, blanditiis facere, qui debitis porro ipsam iste fuga mollitia dignissimos magnam assumenda harum ratione consequuntur
+          repudiandae sunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate modi quo corporis libero, aperiam expedita esse saepe? Nam
+          repudiandae deleniti impedit suscipit laboriosam possimus qui? Ipsa, provident? Corrupti, doloremque? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Similique, accusantium delectus consequatur, maiores tenetur laboriosam quam libero maxime vitae ab facilis distinctio eos eum, obcaecati architecto
+          voluptatum rem in velit.
+        </p>
       </v-container>
     </v-content>
-
-    <v-footer app>
-      <span>&copy; 2019</span>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2020</span>
     </v-footer>
   </v-app>
 </template>
@@ -78,24 +157,13 @@
 <script>
 export default {
   props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-    created () {
-      this.$vuetify.theme.dark = false
-    },
-  }
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
