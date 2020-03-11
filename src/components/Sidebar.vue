720590474992544422702d745132ac856af56ea0
Geylian lingier <geylian.l@gmail.com>
2020-03-11 09:55:53 +0100

    change sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,33 +1,35 @@
 <template>
     <div>
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-        clipped
-      >
+      <v-navigation-drawer v-model="drawer" app temporary>
       <v-list dense>
-         <v-list-item link>
+        <v-list-item link>
           <v-list-item-action>
-           <v-icon>mdi-log-in</v-icon>
+            <v-icon>mdi-login</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title><router-link to="/login">Login</router-link></v-list-item-title>
+            <v-list-item-title>
+              <router-link to="/login">Login/Registration</router-link>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+            <v-list-item-title>
+              <router-link to="/home">Home</router-link>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
+            <v-icon>mdi-information-outline</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+            <v-list-item-title>
+              <router-link to="/about">About</router-link>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
