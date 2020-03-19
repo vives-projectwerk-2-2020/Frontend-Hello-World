5c40980ac7d24fa1c1cced6402613d38c2486e07
Matthias Willem <willemmatthias@gmail.com>
2020-03-19 12:43:05 +0100

    fixed all problems by pulling from the master :(

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,9 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
-      <v-list dense>
-         <v-list-item link>
-          <v-list-item-action>
-           <v-icon>mdi-log-in</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Login</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      clipped-left
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Particula</v-toolbar-title>
-    </v-app-bar>
-
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-
-          <v-container id="app">
-            <HelloWorld msg="Welcome to Particula, now updating dynamically from github"/>
-          </v-container>
-          
-        </v-row>
-      </v-container>
-    </v-content>
-
-    <v-footer app>
-      <span>&copy; Particula 2020</span>
-    </v-footer>
-      <!-- <div id="app">
-        <HelloWorld msg="Welcome to Particula"/>
-      </div> -->
-  </v-app>
-
-
-
+    <div id="app">
+    <v-app id="inspire">
+      <router-view />
+    </v-app>
+  </div>
 </template>
 
 <script>
