e22bfadfe11b0a634828954e9bbc441c496de1bc
Jibe Defoor <jibe.defoor@hotmail.com>
2020-03-25 23:45:16 +0100

    why doesnt anything appear on the page

--- src/views/ChartPage.vue
+++ src/views/ChartPage.vue
@@ -19,6 +19,20 @@
         </v-row>
       </v-container>
     </v-content>
+    <v-layout row wrap>
+     <v-flex xs12 sm6 md4 lg3 v-for="parameter in stats" v-bind:key= "parameter.title">
+       <v-card flat class="text-xs-center ma-40">
+         <v-responsive class="pt-4">
+           icon comes here
+         </v-responsive>
+         <v-card-text>
+           <div class="subheading"> {{ parameter.title }} </div>
+           <div class="subheading"> {{ parameter.value }} </div>
+         </v-card-text>
+        </v-card>
+     </v-flex>
+   </v-layout>
+
 
     <v-footer app>
       <span>&copy; Particula 2020</span>
@@ -48,6 +62,42 @@ export default {
     created () {
       this.$vuetify.theme.dark = false
     },
+    dataForCards() {
+    return {
+      stats: [
+        {
+         // type: "warning",
+          // icon: "ti-shine",
+          title: "Temperature",
+          value: "info.data[0].pm10",
+        },
+        {
+          // type: "success",
+          // icon: "ti-paint-bucket",
+          title: "humidity",
+          value: "60%",
+        },
+        {
+          // type: "danger",
+          // icon: "ti-vector",
+          title: "pressure",
+          value: "1 at",
+        },
+        {
+          // type: "info",
+          // icon: "ti-line-dotted",
+          title: "PM 2.5",
+          value: "10 ppm",
+        },
+        {
+          // type: "info",
+          // icon: "ti-more",
+          title: "PM 10",
+          value: "100ppm",
+        }
+      ],
+    }
+  }
   }
 </script>
 
--- src/views/Live.vue
+++ src/views/Live.vue
@@ -3,22 +3,20 @@
 
     <Sidebar></Sidebar>
 
-    <div class="row">
-      <div class="col-md-6 col-xl-3" v-for="stats in statsCards" :key="stats.title">
-        <stats-card>
-          <div class="icon-big text-center" :class="`icon-${stats.type}`" slot="header">
-            <i :class="stats.icon"></i>
-          </div>
-          <div class="numbers" slot="content">
-            <p>{{stats.title}}</p>
-            {{stats.value}}
-          </div>
-          <div class="stats" slot="footer">
-            <i :class="stats.footerIcon"></i> {{stats.footerText}}
-          </div>
-        </stats-card>
-      </div>
-    </div>
+   <v-layout row wrap>
+     <v-flex xs12 sm6 md4 lg3 v-for="parameter in stats" v-bind:key= "parameter.title">
+       <v-card flat class="text-xs-center ma-40">
+         <v-responsive class="pt-4">
+           icon comes here
+         </v-responsive>
+         <v-card-text>
+           <div class="subheading"> {{ parameter.title }} </div>
+           <div class="subheading"> {{ parameter.value }} </div>
+         </v-card-text>
+        </v-card>
+     </v-flex>
+   </v-layout>
+     
 
     <v-content>
       <v-container class="fill-height" fluid>
@@ -62,7 +60,7 @@ export default {
   name: "App",
   el: "#app",
   components: {
-    Sidebar
+    Sidebar,
   },
   props: {
     source: String
@@ -89,126 +87,45 @@ export default {
         this.getAPI();
       }, 2000);
     }
-  }
-};
-</script>
-<script>
-import { StatsCard, ChartCard } from "@/components/index";
-import Chartist from 'chartist';
-export default {
-  components: {
-    StatsCard,
-    ChartCard
   },
-  /**
-   * Chart data used to render stats, charts. Should be replaced with server data
-   */
-  data() {
+  dataForCards() {
     return {
-      statsCards: [
+      stats: [
         {
-          type: "warning",
-          icon: "ti-shine",
+         // type: "warning",
+          // icon: "ti-shine",
           title: "Temperature",
-          value: info.data[0].pm10,
+          value: "info.data[0].pm10",
         },
         {
-          type: "success",
-          icon: "ti-paint-bucket",
+          // type: "success",
+          // icon: "ti-paint-bucket",
           title: "humidity",
           value: "60%",
         },
         {
-          type: "danger",
-          icon: "ti-vector",
+          // type: "danger",
+          // icon: "ti-vector",
           title: "pressure",
           value: "1 at",
         },
         {
-          type: "info",
-          icon: "ti-line-dotted",
+          // type: "info",
+          // icon: "ti-line-dotted",
           title: "PM 2.5",
           value: "10 ppm",
         },
         {
-          type: "info",
-          icon: "ti-more",
+          // type: "info",
+          // icon: "ti-more",
           title: "PM 10",
           value: "100ppm",
         }
       ],
-      usersChart: {
-        data: {
-          labels: [
-            "9:00AM",
-            "12:00AM",
-            "3:00PM",
-            "6:00PM",
-            "9:00PM",
-            "12:00PM",
-            "3:00AM",
-            "6:00AM"
-          ],
-          series: [
-            [287, 385, 490, 562, 594, 626, 698, 895, 952],
-            [67, 152, 193, 240, 387, 435, 535, 642, 744],
-            [23, 113, 67, 108, 190, 239, 307, 410, 410]
-          ]
-        },
-        options: {
-          low: 0,
-          high: 1000,
-          showArea: true,
-          height: "245px",
-          axisX: {
-            showGrid: false
-          },
-          lineSmooth: Chartist.Interpolation.simple({
-            divisor: 3
-          }),
-          showLine: true,
-          showPoint: false
-        }
-      },
-      activityChart: {
-        data: {
-          labels: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "Mai",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec"
-          ],
-          series: [
-            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
-            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
-          ]
-        },
-        options: {
-          seriesBarDistance: 10,
-          axisX: {
-            showGrid: false
-          },
-          height: "245px"
-        }
-      },
-      preferencesChart: {
-        data: {
-          labels: ["62%", "32%", "6%"],
-          series: [62, 32, 6]
-        },
-        options: {}
-      }
-    };
+    }
   }
 };
+
 </script>
 
 <style>
