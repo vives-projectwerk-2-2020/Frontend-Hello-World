e5b0edc93d277c5dd80dc652543e531db02e7536
Matthias Willem <willemmatthias@gmail.com>
2020-03-26 11:30:05 +0100

    Fixed the sidebar, no direct click on link and no ugly font

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,71 +2,60 @@
   <div>
     <v-navigation-drawer v-model="drawer" app temporary>
       <v-list dense>
-        <v-list-item link>
+        <v-list-item to="/">
           <v-list-item-action>
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/">Home</router-link>
-            </v-list-item-title>
+            <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item to="/login">
           <v-list-item-action>
             <v-icon>mdi-login</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/login">Login/SignUp</router-link>
-            </v-list-item-title>
+            <v-list-item-title>Login/SignUp</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item to="/charts">
           <v-list-item-action>
             <v-icon>mdi-chart-areaspline</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/charts">Sensor Charts</router-link>
-            </v-list-item-title>
+            <v-list-item-title>Sensor Charts</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item to="/live">
           <v-list-item-action>
             <v-icon>mdi-clock-check-outline</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/live">Live</router-link>
-            </v-list-item-title>
+            <v-list-item-title>Live</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item to="/about">
           <v-list-item-action>
             <v-icon>mdi-information-outline</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/about">About</router-link>
-            </v-list-item-title>
+            <v-list-item-title>About</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item to="/settings">
           <v-list-item-action>
             <v-icon>mdi-wrench</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>
-              <router-link to="/settings">Settings</router-link>
-            </v-list-item-title>
+            <v-list-item-title>Settings</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app clipped-left>
