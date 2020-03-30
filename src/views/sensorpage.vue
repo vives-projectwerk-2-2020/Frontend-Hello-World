4fce7d6d6c84ee95ec31705f8632d3c94e6f913d
Jibe Defoor <jibe.defoor@hotmail.com>
2020-03-30 23:41:14 +0200

    added the first cards in the sensorpage

--- src/views/sensorpage.vue
+++ src/views/sensorpage.vue
@@ -9,14 +9,44 @@
     <v-btn rounded color="success" dark class="button">sensor info</v-btn>
     </div>
 
-    <v-card>
+     
+    <v-card
+    class="cards"
+    max-width="344"
+    outlined>
+      <div class="temperature">
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1 text">Temperature</v-list-item-title>
+            <v-list-item-title class="headline mb-1 text">{{ info.data[0].temperature }}°C</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
     </v-card>
+
+    <v-card
+    class="cards"
+    max-width="344"
+    outlined>
+      <div class="humidity">
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1 text">Humidity</v-list-item-title>
+            <v-list-item-title class="headline mb-1 text">{{ info.data[0].humidity }}%</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
+    </v-card>
+    
+     
 
     </v-app>
 </template>
 
 <script>
 import Sidebar from "../components/Sidebar";
+import axios from "axios";
+
 export default {
     name: "sensorpage",
     el: "#sensorpage",
@@ -31,6 +61,22 @@ export default {
   }),
   created() {
     this.$vuetify.theme.dark = false;
+  },
+  mounted() {
+    this.getAPI();
+    this.intervalFetchData();
+  },
+  methods: {
+    getAPI: function() {
+      axios
+        .get("https://virtserver.swaggerhub.com/sillevl/Particula/0.1/measurements/3fa85f64-5717-4562-b3fc-2c963f66afa6?period=24h&properties=pm10")
+        .then(response => (this.info = response));
+    },
+    intervalFetchData: function() {
+      setInterval(() => {
+        this.getAPI();
+      }, 2000);
+    }
   }
 };
 
@@ -45,6 +91,30 @@ export default {
 
 .button:hover{
     opacity: 100%;
+}
+
+.cards{
+  margin: 30px;
+}
+
+.temperature{
+  background-color: rgb(139,0,0,0.6);
+  border-width: 10px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+}
+
+.humidity{
+  background-color: rgb(0,68,208,0.6);
+  border-width: 10px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+}
+
+.text{
+  color:white;
 }
 
 </style>
