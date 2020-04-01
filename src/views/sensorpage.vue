b0e61805425177ff0536c2345836e0b3e3e347e6
Jibe Defoor <jibe.defoor@hotmail.com>
2020-04-01 13:11:48 +0200

    added charts

--- src/views/sensorpage.vue
+++ src/views/sensorpage.vue
@@ -1,81 +1,94 @@
 <template>
-    <v-app id= "inspire">
-
-    <Sidebar></Sidebar>
+  <v-app id= "inspire">
 
     <div>
-    <v-btn rounded color="primary" dark class="button">sensor info</v-btn>
-    <v-btn rounded color="warning" dark class="button">live values</v-btn>
-    <v-btn rounded color="success" dark class="button">sensor info</v-btn>
+      <v-btn rounded color="primary" dark class="button">sensor info</v-btn>
+      <v-btn rounded color="warning" dark class="button">live values</v-btn>
+      <v-btn rounded color="success" dark class="button">sensor info</v-btn>
     </div>
 
-     
-    <v-card
-    class="cards"
-    max-width="344"
-    outlined>
-      <div class="temperature">
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1 text">Temperature</v-list-item-title>
-            <v-list-item-title class="headline mb-1 text">{{ info.data[0].temperature }}°C</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </div>
-    </v-card>
+    <v-layout wrap> 
+      <v-flex
+        v-for="parameter in parameters"
+        :key="parameter.title"
+      >
+        <v-card
+        class="cardss"
+        >
+          <div 
+          :class="parameter.title"
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="headline mb-1 text">{{ parameter.title }}</v-list-item-title>
+                <v-list-item-title class="headline mb-1 text">{{ info.temperature }}°C</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-card>
+      </v-flex>
+   </v-layout>
 
-    <v-card
-    class="cards"
-    max-width="344"
-    outlined>
-      <div class="humidity">
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1 text">Humidity</v-list-item-title>
-            <v-list-item-title class="headline mb-1 text">{{ info.data[0].humidity }}%</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </div>
-    </v-card>
-    
-     
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-container id="sensorpage">
+          <h1>Sensor Charts</h1>
+          <SensorCharts title="Sensor Page" />
+        </v-container>
+      </v-row>
+    </v-container>
+  </v-content>
 
     </v-app>
 </template>
 
 <script>
-import Sidebar from "../components/Sidebar";
 import axios from "axios";
+import SensorCharts from '../components/SensorCharts'
 
 export default {
-    name: "sensorpage",
-    el: "#sensorpage",
-     props: {
-    source: String
-  },
+  name: "SensorPage",
   components: {
-    Sidebar
+    SensorCharts
   },
   data: () => ({
-    drawer: null
+    info: {},
+    drawer: false,
+    parameters: [
+      { title: 'temperature', value: 'info.temperature'},
+      { title: 'humidity', value: 'info.humidity'},
+      { title: 'pressure', value: 'info.pressure' },
+      { title: 'pm10', value: 'info.pm10' },
+      { title: 'pm2_5', value: 'info.pm2_5'},
+      { title: 'timestamp', value: 'info.timestamp'}
+    ],
+    
   }),
-  created() {
-    this.$vuetify.theme.dark = false;
-  },
   mounted() {
     this.getAPI();
     this.intervalFetchData();
   },
   methods: {
     getAPI: function() {
+      // TODO Use websocket / MQTT instead !!
       axios
-        .get("https://virtserver.swaggerhub.com/sillevl/Particula/0.1/measurements/3fa85f64-5717-4562-b3fc-2c963f66afa6?period=24h&properties=pm10")
-        .then(response => (this.info = response));
+        .get(
+          "https://virtserver.swaggerhub.com/sillevl/Particula/0.1/measurements/3fa85f64-5717-4562-b3fc-2c963f66afa6?period=24h&properties=pm10",
+          { headers: { 'Access-Control-Allow-Origin': '*',} } )
+        .then(response => this.info = response.data[0] )
     },
     intervalFetchData: function() {
-      setInterval(() => {
-        this.getAPI();
-      }, 2000);
+      // TODO Use websocket / MQTT instead !!
+      // setInterval(() => {
+      //   this.getAPI();
+      // }, 2000);
     }
   }
 };
@@ -84,8 +97,11 @@ export default {
 
 <style>
 
+
+
 .button{
-    margin: 20px ;
+    margin-top: 70px ;
+    margin-right: 20px;
     opacity: 50%;
 }
 
@@ -93,28 +109,92 @@ export default {
     opacity: 100%;
 }
 
-.cards{
+.cardss{
+  width: 200px;
+  height: 100px;
   margin: 30px;
+  display: inline-block;
 }
 
 .temperature{
-  background-color: rgb(139,0,0,0.6);
+  background-color: rgb(139,0,0,0.4);
   border-width: 10px;
   border-color: black;
   border-radius: 50px;
   color:  white;
+  width: 200px;
+  height: 100px;
+}
+
+.temperature:hover{
+  background-color: rgb(139,0,0, 0.7);
 }
 
 .humidity{
-  background-color: rgb(0,68,208,0.6);
+  background-color: rgb(0,68,208,0.4);
+  border: 10px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+  width: 200px;
+  height: 100px;
+}
+
+.humidity:hover{
+  background-color: rgb(0,68,208,0.7);
+}
+
+.pressure{
+  background-color: rgb(0,153,0,0.4);
   border-width: 10px;
   border-color: black;
   border-radius: 50px;
   color:  white;
+  width: 200px;
+  height: 100px;
+}
+
+.pressure:hover{
+  background-color: rgb(0,153,0,0.7);
+}
+
+.pm2_5{
+  background-color: rgb(76,0,153,0.6);
+  border-width: 10px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+  width: 200px;
+  height: 100px;
+}
+
+.pm10{
+  background-color: rgb(204,0,204,0.6);
+  border-width: 10px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+  width: 200px;
+  height: 100px;
+}
+
+.timestamp{
+  background-color: rgb(255,128,0,0.6);
+  border-width: 400px;
+  border-color: black;
+  border-radius: 50px;
+  color:  white;
+  width: 200px;
+  height: 100px;
 }
 
 .text{
   color:white;
+}
+
+.borders{
+   border-width: 50px;
+  border-color: black;
 }
 
 </style>
