ebe0239e48c054d7b3875cb9e498c7d53f6d0507
Matthias Willem <willemmatthias@gmail.com>
2020-04-02 22:27:25 +0200

    Added some content

--- src/components/AboutSensor.vue
+++ src/components/AboutSensor.vue
@@ -14,7 +14,31 @@
           ></iframe>
         </v-col>
         <v-col cols="12" sm="6">
+          <v-card-title>Sensor info</v-card-title>
 
+
+
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn text>Share</v-btn>
+
+            <v-btn color="purple" text>Explore</v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
+            </div>
+          </v-expand-transition>
         </v-col>
       </v-row>
     </v-container>
