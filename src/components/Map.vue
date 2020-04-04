3eb665b9f36d9f2c1be10e94cda69f6b69710273
Eric Schuitmaker <ericschuitmaker@gmail.com>
2020-04-04 15:00:59 +0200

    reset to old working map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -12,52 +12,25 @@ import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-import Feature from 'ol/Feature';
-
-import Point from "ol/geom/Point";
-import { Vector as VectorLayer } from "ol/layer";
-import { fromLonLat } from "ol/proj";
-import VectorSource from "ol/source/Vector";
-import { Icon, Style } from "ol/style";
-// import {Fill, Icon, Stroke, Style} from 'ol/style';
 export default {
   name: "Map",
   data: () => ({
     // htmlcontent: '<h1>tjoep</h1>
   }),
   mounted: () => {
-    new Map({
-      target: "demoMap",
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
-        new VectorLayer({
-          source: new VectorSource({
-            features: [
-              new Feature({
-                geometry: new Point(
-                  fromLonLat([2.896372, 44.60240])
-                ).setStyle(
-                  new Style({
-                    image: new Icon({
-                      color: "#8959A8",
-                      crossOrigin: "anonymous",
-                      src: "data/square.svg"
-                    })
-                  })
-                )
-              })
-            ]
+      new Map({
+        target: 'demoMap',
+        layers: [
+          new TileLayer({
+            source: new OSM()
           })
-        })
-      ],
-      view: new View({
-        center: [359273.142197, 6658544.189464], //EPSG:3857 coordinate format
-        zoom: 10
-      })
-    });
-  }
+        ],
+        view: new View({
+          center: [359273.142197,6658544.189464],//EPSG:3857 coordinate format
+          zoom: 10
+        }),
+      });
+    }
 };
 </script>
 
