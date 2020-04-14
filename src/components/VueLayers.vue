e281b6cf44927060a1f5483494d4a4fff60cc1c4
Eric Schuitmaker <ericschuitmaker@gmail.com>
2020-04-14 11:39:24 +0200

    implement vuelayers map

---
+++ src/components/VueLayers.vue
@@ -0,0 +1,11 @@
+<script>
+import Vue from "vue";
+import VueLayers from "vuelayers";
+import "vuelayers/lib/style.css"; // needs css-loader
+
+Vue.use(VueLayers);
+Vue.use(VueLayers, {
+  dataProjection: "EPSG:4326"
+});
+export default {};
+</script>
--- src/views/HomeMap.vue
+++ src/views/HomeMap.vue
@@ -1,42 +1,51 @@
 <template>
-  <v-container>
-    <div
-      id="demoMap"
-      style="height:250px"
-    />
-  </v-container>
+  <div>
+    <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
+             data-projection="EPSG:4326" style="height: 400px">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+
+      <vl-geoloc @update:position="geolocPosition = $event">
+        <template slot-scope="geoloc">
+          <vl-feature v-if="geoloc.position" id="position-feature">
+            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+            <vl-style-box>
+              <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+            </vl-style-box>
+          </vl-feature>
+        </template>
+      </vl-geoloc>
+
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+    <div style="padding: 20px">
+      Zoom: {{ zoom }}<br>
+      Center: {{ center }}<br>
+      Rotation: {{ rotation }}<br>
+      My geolocation: {{ geolocPosition }}
+    </div>
+  </div>
 </template>
 
 <script>
-// import vuetify from '@/plugins/vuetify'
+//import Vue from '../components/VueLayers.vue'
+import Vue from 'vue'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css' // needs css-loader
 
-
-export default {
-  name: 'App',
-  mounted() {
-    // let recaptchaScript = document.createElement('script')
-    // recaptchaScript.setAttribute('src', '../components/map/OpenLayers.js')
-    // document.head.appendChild(recaptchaScript)
-
-    
-  },
-  created () {
-    this.$vuetify.theme.dark = false;
-
-    // let ckeditor = document.createElement('script');
-    // ckeditor.setAttribute('src',"../components/map/OpenLayers.js");
-    // document.head.appendChild(ckeditor);
-
-    // var map = new OpenLayers.Map("demoMap");
-    // map.addLayer(new OpenLayers.Layer.OSM());
-    // map.zoomToMaxExtent();
-
-    
-  },
+Vue.use(VueLayers)
+Vue.use(VueLayers, {
+  dataProjection: "EPSG:4326"
+});
+  export default {
+    data () {
+      return { 
+        zoom: 2,
+        center: [0, 0],
+        rotation: 0,
+        geolocPosition: undefined,
+      }
+    },
   }
-
-
 </script>
-
-<style scoped>
-</style>
