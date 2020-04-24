d274108537da977fa14318f347b96d5493c0e94e
Jibe Defoor <jibe.defoor@hotmail.com>
2020-04-24 16:25:58 +0200

    Merge branch 'develop' of github.com:vives-projectwerk-2-2020/Frontend into newsensorpage

---
+++ src/views/TestPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <h1>TestPage</h1>
+    <AboutSensor />
+    <h1>TestPage</h1>
+  </v-app>
+</template>
+
+<script>
+import AboutSensor from "../components/AboutSensor";
+export default {
+  name: "TestPage",
+  components: {
+    AboutSensor
+  }
+};
+</script>
+
+<style scoped>
+</style>
